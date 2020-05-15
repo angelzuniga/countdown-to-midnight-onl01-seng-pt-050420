24c9af9310c63c58ec09c53b8966d52ca9eae4c6
Angel Zuniga <angelzuniga@outlook.com>
2020-05-15 06:20:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,10 +9,11 @@ def countdown(number)
 end
 
 
-# ef countdown(seconds_to_midnight)
-#   while seconds_to_midnight > 0
-#     puts "#{seconds_to_midnight} SECOND(S)!"
-#     seconds_to_midnight -= 1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
+def countdown_with_sleep(number)
+  while number > 10 
+  puts "#{number} SECOND(S)!"
+  sleep 1 
+  number -= 1 
+  end
+ return "HAPPY NEW YEAR"
+end
