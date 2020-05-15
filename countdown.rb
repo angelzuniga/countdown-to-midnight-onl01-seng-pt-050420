ac23d5b3af001f83d4b415922f90baec54a9fa50
Angel Zuniga <angelzuniga@outlook.com>
2020-05-15 06:16:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,19 +1,18 @@
 
 
 def countdown(number)
-  until number == 0
-  puts "#{number} SECOND(S) !"
-   
-  number += -1 
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1 
   return "HAPPY NEW YEAR!"
   end
 end
 
 
-ef countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
+# ef countdown(seconds_to_midnight)
+#   while seconds_to_midnight > 0
+#     puts "#{seconds_to_midnight} SECOND(S)!"
+#     seconds_to_midnight -= 1
+#   end
+#   "HAPPY NEW YEAR!"
+# end
